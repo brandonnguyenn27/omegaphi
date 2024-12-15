cd48e67bc2430ed13dbf5677d3c94aaf9c6b8429
brandonnguyenn27 <brandonnguyenn27@gmail.com>
2024-12-14 16:52:28 -0800

    sidebar + icons

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -1,10 +1,34 @@
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import { HomeIcon, DashboardIcon, CalendarIcon } from "@radix-ui/react-icons";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: DashboardIcon,
+  },
+  {
+    title: "Events",
+    url: "/admin/events",
+    icon: CalendarIcon,
+  },
+];
 
 export function AppSidebar() {
   return (
@@ -12,6 +36,21 @@ export function AppSidebar() {
       <SidebarHeader />
       <SidebarContent>
         <SidebarGroup />
+        <SidebarGroupLabel>Application</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon className="h-4 w-4 mr-2" />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
