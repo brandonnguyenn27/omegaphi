93a77207a004314a1577a47e034c39e7c1b676b9
Brandon Nguyen <brandonnguyenn27@gmail.com>
2024-12-30 19:17:20 -0800

    rushee availability actions

--- actions/admin/rushee.ts
+++ actions/admin/rushee.ts
@@ -31,3 +31,33 @@ export async function updateRusheeAction(payload: {
   const supabase = await createClient();
   await supabase.from("rushees").update(fields).eq("id", id);
 }
+export async function addRusheeAvailability(formData: FormData) {
+  const supabase = await createClient();
+
+  const rushee_id = formData.get("rushee_id") as string;
+  const start_time = formData.get("start_time") as string;
+  const end_time = formData.get("end_time") as string;
+
+  await supabase
+    .from("rushee_availabilities")
+    .insert({ rushee_id, start_time, end_time });
+}
+export async function deleteRusheeAvailability(rusheeId: string) {
+  const supabase = await createClient();
+  await supabase
+    .from("rushee_availabilities")
+    .delete()
+    .eq("rushee_id", rusheeId);
+}
+
+export async function updateRusheeAvailability(payload: {
+  id: string;
+  rushee_id: string;
+  start_time: string;
+  end_time: string;
+}) {
+  const { id, ...fields } = payload;
+
+  const supabase = await createClient();
+  await supabase.from("rushee_availabilities").update(fields).eq("id", id);
+}
