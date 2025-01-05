2c444e6a78c811b1d5ac38b21c6c2adb9e28ce36
Brandon Nguyen <brandonnguyenn27@gmail.com>
2025-01-05 15:35:35 -0800

    fix hydration error

--- components/admin/interviews/AddUserAvailabilityModal.tsx
+++ components/admin/interviews/AddUserAvailabilityModal.tsx
@@ -49,7 +49,9 @@ export default function AddUserAvailabilityModal({
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger>
-        <Button>Add Availability</Button>
+        <div>
+          <Button>Add Availability</Button>
+        </div>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
