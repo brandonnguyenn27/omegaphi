3d05f7bd6fb64721d109fb816ef694554ee097ee
Brandon Nguyen <brandonnguyenn27@gmail.com>
2025-01-13 18:38:49 -0800

    page props for view

--- app/(authenticated)/admin/rushees/view/page.tsx
+++ app/(authenticated)/admin/rushees/view/page.tsx
@@ -1,10 +1,10 @@
 import RusheeDetails from "@/components/admin/rushee/RusheeDetails";
 
-export default async function Page({
-  searchParams,
-}: {
+interface PageProps {
   searchParams: { rusheeId?: string };
-}) {
+}
+
+export default async function Page({ searchParams }: PageProps) {
   const rusheeId = searchParams.rusheeId;
   if (!rusheeId) {
     return <p>No rushee ID provided.</p>;
